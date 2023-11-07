ccf5339adfa8d4ebbf5c944004b25e345b6f6242
doukheeWon <fain9301@candh.co.kr>
2023-11-07 19:01:17 +0900

    feat: kafka setting

---
+++ rentalCardService/src/main/java/kr/co/won/rentalcardservice/adapter/kafka/RentalKafkaProducer.java
@@ -0,0 +1,88 @@
+package kr.co.won.rentalcardservice.adapter.kafka;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import kr.co.won.rentalcardservice.appilcation.port.out.EventOutputPort;
+import kr.co.won.rentalcardservice.domain.model.event.ItemRented;
+import kr.co.won.rentalcardservice.domain.model.event.ItemReturned;
+import kr.co.won.rentalcardservice.domain.model.event.OverDueCleared;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@Component
+public class RentalKafkaProducer implements EventOutputPort {
+
+    @Value("${producers.topic1.name}")
+    private String TOPIC_RENT;
+    @Value("${producers.topic2.name")
+    private String TOPIC_RETURN;
+
+    @Value("${producers.topic3.name}")
+    private String TOPIC_OVER_DUE_CLEAR;
+
+    private final KafkaTemplate<String, ItemRented> rentalKafka;
+    private final KafkaTemplate<String, ItemReturned> returnKafka;
+    private final KafkaTemplate<String, OverDueCleared> overdueClearKafka;
+
+    public RentalKafkaProducer(KafkaTemplate<String, ItemRented> rentalKafka, KafkaTemplate<String, ItemReturned> returnKafka, KafkaTemplate<String, OverDueCleared> overdueClearKafka) {
+        this.rentalKafka = rentalKafka;
+        this.returnKafka = returnKafka;
+        this.overdueClearKafka = overdueClearKafka;
+    }
+
+    @Override
+    public void occurRentalEvent(ItemRented rentedEvent) throws JsonProcessingException {
+        // kafka 에서 전달 후 반환 값을 가져오는 것
+        CompletableFuture<SendResult<String, ItemRented>> future = rentalKafka.send(TOPIC_RENT, rentedEvent);
+        // TODO checking add callback
+        future.whenComplete((result, throwable) -> {
+            // result 성공시
+            if (result != null) {
+                ItemRented resultItem = result.getProducerRecord().value();
+                log.info("[kafka-success] send message = [" + resultItem.getItem().getNo() + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+            // error  발생 시 throwable
+            if (throwable != null) {
+                log.error("[kafka-error]Unable to send message = [" + rentedEvent.getItem().getNo() + "] due to : " + throwable.getMessage(), throwable);
+            }
+        });
+
+    }
+
+    @Override
+    public void occurReturnEvent(ItemReturned itemReturnedEvent) throws JsonProcessingException {
+        CompletableFuture<SendResult<String, ItemReturned>> future = returnKafka.send(TOPIC_RETURN, itemReturnedEvent);
+        future.whenComplete((result, throwable) -> {
+            // result 성공시
+            if (result != null) {
+                ItemReturned resultItem = result.getProducerRecord().value();
+                log.info("[kafka-success] send message = [" + resultItem.getItem().getNo() + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+            // error  발생 시 throwable
+            if (throwable != null) {
+                log.error("[kafka-error]Unable to send message = [" + itemReturnedEvent.getItem().getNo() + "] due to : " + throwable.getMessage(), throwable);
+            }
+        });
+    }
+
+    @Override
+    public void occurOverdueClearEvent(OverDueCleared overDueCleared) throws JsonProcessingException {
+        CompletableFuture<SendResult<String, OverDueCleared>> future = overdueClearKafka.send(TOPIC_OVER_DUE_CLEAR, overDueCleared);
+        future.whenComplete((result, throwable) -> {
+            // result 성공시
+            if (result != null) {
+                OverDueCleared resultItem = result.getProducerRecord().value();
+                log.info("[kafka-success] send message = [" + resultItem.getIdName().getId() + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+            // error  발생 시 throwable
+            if (throwable != null) {
+                log.error("[kafka-error]Unable to send message = [" + overDueCleared.getIdName().getId() + "] due to : " + throwable.getMessage(), throwable);
+            }
+        });
+    }
+}
--- rentalCardService/src/main/java/kr/co/won/rentalcardservice/config/KafkaConfiguration.java
+++ rentalCardService/src/main/java/kr/co/won/rentalcardservice/config/KafkaConfiguration.java
@@ -1,32 +1,38 @@
 package kr.co.won.rentalcardservice.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @Configuration
 public class KafkaConfiguration {
-/*
+
+    @Value("${producers.bootstrap-servers}")
+    private String boostrapServers;
+
     @Bean
-    public ProducerFactory<String, ItemRental> producerFactory() {
-        Map<String, Object> configuration = new HashMap<>();
-        configuration.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "");
-        configuration.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "");
-        configuration.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "");
-        return new DefaultKafkaProducerFactory<>(configuration);
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers);
+        // key 값을 직렬화 하는 설정
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        // kafka value 에 대한 직렬화 설정
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+        return new DefaultKafkaProducerFactory(config);
     }
 
     @Bean
-    public KafkaTemplate kafkaTemplate(ProducerFactory producerFactory){
+    public KafkaTemplate kafkaTemplate(ProducerFactory producerFactory) {
         return new KafkaTemplate(producerFactory);
     }
-*/
-
-
 }
